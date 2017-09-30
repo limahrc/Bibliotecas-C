e25c3cd27285548a9380129211eba6f9897423ff
limahrc <limahrc@gmail.com>
2017-09-29 22:25:25 -0300

    Adição de funções de lista encadeada

--- ints/ints.c
+++ ints/ints.c
@@ -1,5 +1,13 @@
 #include "ints.h"
 
+struct int_node {
+	int info;
+	node* prox;
+	node* ant;
+};
+
+#define null_ptr_msg printf("hrc: the refered address is null.\n")
+
 int hvalue(int *vec, int tam){
 	int higherv;
 	higherv = vec[0];
@@ -42,4 +50,104 @@ int* vecsum(int *vec1, int *vec2, int tam){
 bool isEven(int x){
   if(x%2==0) return true;
   else return false;
+}
+
+void* make(){
+	return NULL;
+}
+
+node* newnode(int x){
+	node* n = (node*) calloc(1, sizeof(node));
+	n->info = x;
+	return n;
+}
+
+static node* last(lint l){
+  if (l!=NULL) {
+    while(l->prox!=NULL) l = l->prox;
+    return l;
+  }
+  return NULL;
+}
+
+static node* previous(lint l, node* n){
+	if (l!=NULL) {
+		while(l->prox!=n) l = l->prox;
+		return l;
+	}
+	return NULL;
+}
+
+void insend(lint *l, node* novo){
+	if(l!=NULL){
+  	if(*l==NULL) *l = novo;
+   	else {
+  		node *aux = *l;
+  		aux = last(*l);
+  		aux->prox = novo;
+  	}
+  } else null_ptr_msg;
+}
+
+void instop(lint *l, node *novo){
+	if(l!=NULL){
+  	if(*l==NULL) *l = novo;
+  	else {
+  		novo->prox = *l;
+  		*l = novo;
+  	}
+  } else null_ptr_msg;
+}
+
+lint insvec(int *vec, int size){
+	lint l = make();
+	int i;
+	for (i=0; i<size; i++) insend(&l, newnode(vec[i]));
+	return l;
+}
+
+void unmake(lint *l) {
+	node* aux = NULL;
+	while((*l)->prox!=NULL){
+		aux = *l;
+		*l = (*l)->prox;
+		free(aux);
+	}
+	free(*l);
+	*l = NULL; //Só pra garantir
+}
+
+int getinfo(node* n){
+	if(n!=NULL){
+		return n->info;
+	}
+}
+
+bool setinfo(node* n, int info){
+	if(n!=NULL){
+		n->info = info;
+		return true;
+	}
+	else return false;
+}
+
+node* getprox(node* n){
+	if(n!=NULL){
+		return n->prox;
+	}
+}
+
+bool setprox(node* n, node* m){
+	if(n!=NULL){
+		n->prox = m;
+		return true;
+	}
+	else return false;
+}
+
+void show(lint l){
+	while (l!=NULL) {
+		printf("%i ", l->info);
+		l = l->prox;
+	}
 }
--- ints/ints.h
+++ ints/ints.h
@@ -2,7 +2,8 @@
 #define _INTS_H
 
 /*
-	BIBLIOTECA DE FUNÇÕES PARA INTEIROS E VETORES DE INTEIROS
+	BIBLIOTECA DE FUNÇÕES PARA INTEIROS, VETORES DE INTEIROS E LISTAS ENCADEADAS
+  DE INTEIROS
 	AUTOR: HERICK LIMA NUNES
 	EMAIL: LIMAHRC@GMAIL.COM
 	ENGENHARIA DE COMPUTAÇÃO
@@ -14,6 +15,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+
+typedef struct int_node node;
+typedef node* lint;
 
 /*
 * Identifica qual é o elemento de maior valor em um vetor
@@ -46,5 +50,73 @@
 /*
 * Verifica se um número é par
 */ bool isEven(int x);
+
+/*
+* Prepara a lista para ser montada (ainda sem corpo).
+*/ void* make();
+
+/*
+* Aloca um novo nó e retorna um ponteiro para o mesmo.
+* @dado: ponteiro para o dado a ser armazenado
+*/ node* newnode(int x);
+
+/*
+* Obtém o endereço do último nó da lista.
+*/ static node* last(lint l);
+
+/*
+* Obtém o endereço do nó anterior ao nó parâmetro.
+*/ static node* previous(lint l, node* n);
+
+/*
+* Insere um novo nó no fim da lista passada
+* como parâmetro.
+* @l: lista na qual se deseja inserir
+* @novo: ponteiro para o nó o qual se deseja inserir
+*/ void insend(lint *l, node* novo);
+
+/*
+* Insere um novo nó no início da lista passada
+* como parâmetro.
+* @l: lista na qual se deseja inserir
+* @novo: ponteiro para o nó o qual se deseja inserir
+*/ void instop(lint *l, node *novo);
+
+/*
+* Cria uma lista a partir de um vetor parâmetro.
+  @vec: vetor de inteiros
+  @size: tamanho do vetor
+*/ lint insvec(int *vec, int size);
+
+/*
+* Percorre a lista desalocando seus nós da memória.
+* @l: endereço da lista que se deseja desfazer
+*/ void unmake(lint *l);
+
+/*
+*	Obtém o endereço de memória de objeto armazenado no nó
+*	passado como parâmetro.
+*/ int getinfo(node* n);
+
+/*
+* Define a informação armazenada (ponteiro) no nó parâmetro e
+* retorna true se a operação for bem sucedida ou false, caso contrário.
+* @info: endereço do objeto que se deseja armazenar
+*/ bool setinfo(node* n, int info);
+
+/*
+* Obtém o endereço do próximo nó ligado ao nó parâmetro.
+*/ node* getprox(node* n);
+
+/*
+* Define o endereço do próximo nó ligado ao nó parâmetro.
+* retorna true se @n puder apontar para @m corretamente
+* ou retorna false, caso contrário
+*/ bool setprox(node* n, node* m);
+
+/*
+* Percorre e exibe as informações de todos os nós da lista.
+* @l: lista que se deseja percorrer
+*/ void show(lint l);
 
 #endif /* end of include guard: _INTS_H_ */
